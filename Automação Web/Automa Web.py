e255599dc302fed943514576ed649fd99714aa8c
Alex Stéfano Lopes <alexstefanolopes@gmail.com>
2024-10-23 14:06:31 -0300

    V

--- Automação Web/Automa Web.py
+++ Automação Web/Automa Web.py
@@ -7,3 +7,6 @@ navegador.get('https://ri.magazineluiza.com.br/')
 
 #O Jupyter falar que find_element_by_xpath é obsoleto e diz para usar apenas find_element, mas não deu certo, mesmo copiando apenas como elemento o botão
 navegador.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/div/div/a[2]/span').click()
+
+
+TESTE_NOVO
